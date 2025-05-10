6c225bf49520ac1fcb689f00e85488ee47bb4d43
konyshevgmbh <konyshevgmbh@users.noreply.github.com>
2025-05-10 18:10:25 +0000

    deploy: c98dfcdbef63de08af53668bac688dd6d80bb527

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2408464001"
+    serviceWorkerVersion: "2233869744"
   }
 });
