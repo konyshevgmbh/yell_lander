be19474740a9308fff2abe433f206235be0f6ed6
igor.konyshev <konyshevgmbh@gmail.com>
2025-05-11 22:51:20 +0200

    Bump version to 0.3.0 and update versionCode to 3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
+        versionCode = 3
         versionName = flutter.versionName
     }
     signingConfigs {
